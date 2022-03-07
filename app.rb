fd346b0e565a195e088625bc2b73f1da11001d74
simon-lofgren-nti-johanneberg <74712595+simon-lofgren-nti-johanneberg@users.noreply.github.com>
2022-03-07 22:20:06 +0100

    Fixed some bugs and added new features

--- app.rb
+++ app.rb
@@ -22,56 +22,47 @@ end
 
 #Routes:
 get('/') do
+  session[:error_message_register] = nil
+  session[:error_message_login] = nil
   slim(:home)
 end
 
 get('/register') do
+  session[:error_message_login] = nil
   slim(:register)
 end
 
-get('/empty_register') do
-  slim(:empty_register)
-end
-
-get('/wrong_register_password') do
-  slim(:wrong_register_password)
-end
-
-get ('/username_already_taken') do
-  slim(:username_already_taken)
-end
-
 get('/login') do
+  session[:error_message_register] = nil
   slim(:login)
 end
 
-get('/empty_login') do 
-  slim(:empty_login)
+get('/exercises_and_workouts/:id/delete') do  
+  session[:type] = params[:type_element]
+  slim(:"exercises_and_workouts/delete")
 end
 
-get('/wrong_login_username') do 
-  slim(:wrong_login_username)
+get('/exercises_and_workouts/new') do
+  slim(:"exercises_and_workouts/new")
 end
 
-get('/wrong_login_password') do
-  slim(:wrong_login_password)
-end
-
-get('/my_exercises_and_workouts') do
+get('/exercises_and_workouts') do
+  session[:error_message_new_exercise_or_workout] = nil
+  session[:selected_type] = nil
   db = connection_database('db/workout.db')
   db.results_as_hash = true
   if session[:choosing_filter] == "all" or session[:choosing_filter] == nil
     #Detta kan göras bättre med inner join (
-    @result_workouts = db.execute("SELECT name FROM workouts WHERE user_id = ?", session[:id])
-    @result_exercises = db.execute("SELECT name FROM exercises WHERE user_id = ?", session[:id])
+    @result_workouts = db.execute("SELECT * FROM workouts WHERE user_id = ?", session[:id])
+    @result_exercises = db.execute("SELECT * FROM exercises WHERE user_id = ?", session[:id])
     # )
   elsif session[:choosing_filter] == "exercises"
-    @result_exercises = db.execute("SELECT name FROM exercises WHERE user_id = ?", session[:id])
+    @result_exercises = db.execute("SELECT * FROM exercises WHERE user_id = ?", session[:id])
   else
-    @result_workouts = db.execute("SELECT name FROM workouts WHERE user_id = ?", session[:id])
+    @result_workouts = db.execute("SELECT * FROM workouts WHERE user_id = ?", session[:id])
   end
-  #puts "Workouts: #{@result_workouts}"
-  #puts "Exercises: #{@result_exercises}"
+  # puts "Workouts: #{@result_workouts}"
+  # puts "Exercises: #{@result_exercises}"
   slim(:"exercises_and_workouts/index")
 end
 
@@ -91,22 +82,25 @@ post('/login') do
   password = params[:password]
 
   if username == "" or password == ""
-    redirect('empty_login')
+    session[:error_message_login] = "Your username and/or password can't be empty"
+    redirect('/login')
   else 
     db = connection_database('db/workout.db')
     db.results_as_hash = true
     result = db.execute("SELECT * FROM users WHERE username = ?",username).first
     if result == nil
-      redirect('wrong_login_username')
+      session[:error_message_login] = "Username does not exist"
+      redirect('/login')
     else
       pwdigest = result["pwdigest"]
       id = result["id"]
       if BCrypt::Password.new(pwdigest) == password
         session[:id] = id
         session[:username] = username
-        redirect('/my_exercises_and_workouts')
+        redirect('/exercises_and_workouts')
       else
-        redirect('wrong_login_password')
+        session[:error_message_login] = "Wrong password"
+        redirect('/login')
       end
     end
   end
@@ -119,9 +113,11 @@ post('/register') do
   db = connection_database('db/workout.db')
   result = db.execute("SELECT * FROM users WHERE username = ?",username)
   if username == "" or password == "" or password_confirm == ""
-    redirect('/empty_register')
+    session[:error_message_register] = "One or more of the boxes are empty"
+    redirect('/register')
   elsif result != []
-    redirect('/username_already_taken')
+    session[:error_message_register] = "Username already taken"
+    redirect('/register')
   else
     if password == password_confirm
       password_digest = BCrypt::Password.create(password)
@@ -129,7 +125,8 @@ post('/register') do
       db.execute("INSERT INTO users (username,pwdigest) VALUES (?,?)",username,password_digest)
       redirect('/login')
     else
-      redirect('/wrong_register_password')
+      session[:error_message_register] = "The passwords don't match"
+      redirect('/register')
     end
   end
 end
@@ -140,9 +137,7 @@ post('/logout') do
 end
 
 post('/filter') do
-  session[:filter] = params["filter"]
-  db = connection_database('db/workout.db')
-  db.results_as_hash = true
+  session[:filter] = params[:filter]
     case session[:filter]
       when "all"
         session[:choosing_filter] = "all"
@@ -151,9 +146,50 @@ post('/filter') do
       when "workouts"
         session[:choosing_filter] = "workouts"
     end
-  redirect('/my_exercises_and_workouts')
+  redirect('/exercises_and_workouts')
 end
 
+post('/exercises_and_workouts/:id/delete') do
+  id = params[:id].to_i
+  db = connection_database('db/workout.db')
+  if session[:type] == "exercise"
+    db.execute("DELETE FROM exercises WHERE id = ?",id)
+  else
+    db.execute("DELETE FROM workouts WHERE id = ?",id)
+  end
+  redirect('/exercises_and_workouts')
+end 
 
-#Till nästa gång: Fixa relationen mellan exercises och muscle groups
+post('/exercises_and_workouts/new') do
+  if session[:selected_type] == "exercise"
+    title_exercise = params[:title_exercise]
+    if title_exercise == ""
+      session[:error_message_new_exercise_or_workout] = "Your title can't be empty"
+      redirect('/exercises_and_workouts/new')
+    end
+    muscle_group = params[:muscle_group]
+    puts muscle_group
+    #Inte klar med denna post route
+  else
+
+  end
+
+end
+
+post('/select_type') do 
+  type = params[:type]
+    case type
+      when "select_type"
+        session[:error_message_new_exercise_or_workout] = "You must select a type"
+      when "exercise"
+        session[:error_message_new_exercise_or_workout] = nil
+        session[:selected_type] = "exercise"
+      when "workout"
+        session[:error_message_new_exercise_or_workout] = nil
+        session[:selected_type] = "workout"
+    end
+  redirect('/exercises_and_workouts/new')
+end
+
+#Till nästa gång: Lösa så att det går att välja flera alternativ i en och samma select (new.slim rad 6)
 #Till senare: Fixa before do's 
